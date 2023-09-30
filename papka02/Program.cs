912e4d438288106e59187269c537f66e6abe9453
marina <marinapetrochuk@gmail.com>
2023-09-30 23:47:23 +0300

    papka02

---
+++ papka02/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n==1)
+    Console.WriteLine("Понедельник нет");
+else if (n==2)
+    Console.WriteLine("Вторник нет");
+else if (n==3)
+    Console.WriteLine("Среда нет");
+else if (n==4)
+    Console.WriteLine("Четверг нет");
+else if (n==5)
+    Console.WriteLine("Пятница нет");
+else if (n==6)
+    Console.WriteLine("Суббота да");
+else if (n==7)
+    Console.WriteLine("Воскресение да");
+
