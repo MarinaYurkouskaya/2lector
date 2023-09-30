d1e57a102ffba41ec969d67a5100a6d31a866a61
marina <marinapetrochuk@gmail.com>
2023-09-30 22:24:04 +0300

    papka01

---
+++ papka01/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+if (n >= 100)
+    Console.WriteLine($" {n%10}");
+else
+    Console.Write($" Третьей цифры нет");
+ 
+
