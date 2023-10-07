40ab86d2613c36dd5b40d3abb336d7ce4209346a
marina <marinapetrochuk@gmail.com>
2023-10-08 01:04:23 +0300

    papaka004

---
+++ papka004/Program.cs
@@ -0,0 +1,9 @@
+﻿принимает на вход число (N) и выводит таблицу кубов чисел от 1 до N (включительно) каждое на новой строке.
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine( Math.Pow(i, 3)+ "");
+}
+
